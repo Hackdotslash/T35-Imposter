d800cf0bcbbe4d4681b8065115a733e0ab83aec5
sakshijain009 <62885857+sakshijain009@users.noreply.github.com>
2021-01-30 17:47:43 +0530

    second

--- app.js
+++ app.js
@@ -3,30 +3,77 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
 
-mongoose.connect('mongodb://localhost:27017/rapidRed', {useNewUrlParser: true, useUnifiedTopology: true});
 const app = express();
 
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
+mongoose.connect("mongodb://localhost:27017/rapidred",{useNewUrlParser:true});
 
 
+/*****************SCHEMAS********************/
+const subscribeSchema={
+  email: String
+};
+
+const bloodBankSchema={
+  name: String,
+  email: String,
+  address: String,
+  country: String,
+  state: String,
+  pincode: Number
+};
+
+
+
+
+
+/**************Collections****************/
+const subscribe = mongoose.model("subscribe",subscribeSchema);
+
+
+
+
+
+/*****************************GET REQUESTS****************************/
 app.get("/",(req,res)=>{
-	res.render("register");
+	res.render("request");
 });
 
 app.get("/request",(req,res)=>{
 	res.render("request");
 });
 
+app.get("/register",(req,res)=>{
+	res.render("register");
+});
+
+app.get("/banks",(req,res)=>{
+	res.render("banks");
+});
+
+app.get("/about",(req,res)=>{
+	res.render("about");
+});
+
+/************************POST REQUESTS*******************/
+app.post("/subscribe",(req,res)=>{
+
+	const eid = new subscribe({
+		email: req.body.email
+	});
+	eid.save();
+	res.redirect("/");
+});
 
 
 
 
 
 
-
+/*******************************************************/
 app.listen(3000, function() {
   console.log("Server started on port 3000");
 });
